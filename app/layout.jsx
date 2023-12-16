19c021b5fcc329480676ba21fe6c5cf7565da46c
sujan khatri <sujansince2059@gmail.com>
2023-12-16 21:58:24 +0545

    initializw app

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+export const metadata = {
+  title: "promptopia",
+  description: "search prompts now",
+};
+function Rootlayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
+
+export default Rootlayout;
---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+import Feed from "@components/Feed";
+function Home() {
+  return (
+    <>
+      <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+          Discover and share AI prompts
+          <br className="max-md:hidden" />
+          <span className="orange_gradient text-center">
+            AI Powered Prompts
+          </span>
+        </h1>
+        <p className="desc text-center">
+          promptopia is an good website to get AI prompts.get hands on this also
+          share this app
+        </p>
+        {/* feed */}
+        <Feed />
+      </section>
+    </>
+  );
+}
+
+export default Home;

43f43539cef961dfeaf0812fc1082a7c6d79854d
sujan khatri <sujansince2059@gmail.com>
2023-12-26 17:55:51 +0545

    user connect to db or not

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
 
 console.log({
   clientId: process.env.GOOGLE_ID,
@@ -16,7 +17,16 @@ const handler = NextAuth({
   async session({ session }) {},
   async signIn({ profile }) {
     try {
-    } catch (error) {}
+      await connectToDB();
+
+      // check if user already exists
+
+      // check if user doesnot exists
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
 export { handler as GET, handler as POST };
